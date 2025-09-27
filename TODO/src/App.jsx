ec0a341da68b5c295202a39b4177d991098e86bd
Ansh0305 <saiansh2016@gmail.com>
2025-09-28 00:06:36 +0530

    Initializing TODO roject

---
+++ TODO/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CssBaseline from "@mui/material/CssBaseline";
+
+function App() {
+
+  return (
+    <>
+    <CssBaseline/> 
+    <h2>Todos</h2>
+    </>
+  )
+}
+
+export default App
