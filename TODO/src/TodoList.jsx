b0743af0bcef2c6f6bc4a74c14eea67f902151e3
Ansh0305 <saiansh2016@gmail.com>
2025-09-28 00:53:22 +0530

    Removing Todos

--- TODO/src/TodoList.jsx
+++ TODO/src/TodoList.jsx
@@ -13,14 +13,32 @@ const initialTodos = [
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
   const removeTodo = (id) => {
-    setTodos(prevTodos =>{
-        return prevTodos.filter((t) => t.id !== id);
-    })
-  }
+    setTodos((prevTodos) => {
+      return prevTodos.filter((t) => t.id !== id);
+    });
+  };
+
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      });
+    });
+  };
+  
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       {todos.map((todo) => (
-        <TodoItem todo={todo} key={todo.id} remove={removeTodo}/>
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          remove={removeTodo}
+          toggle={() => toggleTodo(todo.id)}
+        />
       ))}
     </List>
   );
