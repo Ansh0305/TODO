2c37c534525b7626d7292c957e34500a02048fbb
Ansh0305 <saiansh2016@gmail.com>
2025-09-28 00:26:19 +0530

    TodoList Component

--- TODO/src/App.jsx
+++ TODO/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import CssBaseline from "@mui/material/CssBaseline";
+import TodoList from './TodoList';
 
 function App() {
 
   return (
     <>
     <CssBaseline/> 
-    <h2>Todos</h2>
+    <h2>TODOS</h2>
+    <TodoList/>
     </>
   )
 }
